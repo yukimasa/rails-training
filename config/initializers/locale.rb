35bf4107d0e30e743cd00cfdce5f3ce98b26985f
YukimasaIkeda <ykms123456789@gmail.com>
2019-05-30 01:59:23 +0900

    翻訳の設定

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,1 +1,3 @@
 Rails.application.config.i18n.default_locale = :ja
+Rails.application.config.time_zone = 'Asia/Tokyo'
+Rails.application.config.active_record.default_timezone = :local
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_115436) do
+ActiveRecord::Schema.define(version: 2019_05_29_102452) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
