e776cb490ad55c3c26e8d87a81ec9318d4a79e22
YukimasaIkeda <ykms123456789@gmail.com>
2019-05-30 22:46:55 +0900

    ユーザー管理画面の作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,9 +1,21 @@
 class Admin::UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def create
     @user = User.new(user_params)
 
     if @user.save
@@ -11,6 +23,22 @@ class Admin::UsersController < ApplicationController
     else
       render :new
     end
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to admin_users_path(@user), notice: "ユーザー「#{@user.name}」を更新しました。"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path, notice: "ユーザー「#{@user.name}」を削除しました。"
   end
 
   def user_params
