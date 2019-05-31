af131e0cf8385ffa18e253b3a071edde980919af
YukimasaIkeda <ykms123456789@gmail.com>
2019-05-31 11:29:52 +0900

    ログイン機能の実行

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :set_locale
+  helper_method :current_user
 
   private
 
-  def set_locale
-    I18n.locale = params[:locale] || "ja"
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 end
