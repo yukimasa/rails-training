d240860ab7601bd22a25f300127983d86e0bda12
YukimasaIkeda <ykms123456789@gmail.com>
2019-06-03 00:52:34 +0900

    テスト環境構築

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,5 @@
-FactroyBot.define do
-  factroy :user do
+FactoryBot.define do
+  factory :user do
     name { 'テストユーザー' }
     email { 'test1@example.com' }
     password { 'password' }
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,7 +18,7 @@ require 'capybara/rspec'
 
 RSpec.configure do |config|
 
-  config.before(:each, type: system) do
+  config.before(:each, type: :system) do
     driven_by :selenium_chrome_headless
   end
 
