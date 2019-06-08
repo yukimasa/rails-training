aa5845bfb16076ba38646ae657c24392e3b3f036
Yukimasa Ikeda <ykms123456789@gmail.com>
2019-06-09 01:45:18 +0900

    seedファイルの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
   namespace :admin do
     resources :users
   end
+
   root to: 'tasks#index'
   resources :tasks do
     post :confirm, action: :confirm_new, on: :new
