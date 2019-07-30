9efec85b092b2fdb38975ed05d320ceba23a3317
yukimasa ikeda <ykms123456789@gmail.com>
2019-07-30 19:58:11 +0900

    Add confirmation screen

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,6 +15,11 @@ class TasksController < ApplicationController
   def create
     @task = current_user.tasks.new(task_params)
 
+    if params[:back].present?
+      render :new
+      return
+    end
+
     if @task.save
       redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
     else
@@ -33,6 +38,11 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
+  end
+
+  def confirm_new
+    @task = current_user.tasks.new(task_params)
+    render :new unless @task.valid?
   end
 
   private
